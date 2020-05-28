bd0ebcf506f97d4392d042a123e38c52695ce5cd
Dylan <dcren2010@gmail.com>
2020-05-28 10:35:57 -0400

    api and fetchData created, also updata App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import styles from './App.module.css'
+
+// import Cards from './components/Cards';
+// import Chart from './components/Chart';
+// import CountryPicker from './components/CountryPicker'
+
+import {Cards, Chart, CountryPicker} from './components';
+
+import {fetchData} from "./api";
 
 class App extends React.Component{
+
+    async componentDidMount() {
+        const data = await fetchData();
+        console.log(data)
+    }
+
     render() {
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+               <Cards />
+                <CountryPicker />
+               <Chart />
+
             </div>
         )
     }
